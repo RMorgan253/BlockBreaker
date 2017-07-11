13ebf34f2f633a758a510b4b8f85bae340ffa11f
Ryan Morgan <Morganglassworks@gmail.com>
2017-07-10 22:24:40 -0700

    End of Day Backup
    7/10/2017 After lessons

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+	public LevelManager levelManager;
+	
+	void OnTriggerEnter2D (Collider2D trigger) {
+		print ("Trigger");
+		levelManager.LoadLevel("Win");
+	}
+	
+	void OnCollisonEnter2D (Collision2D collision) {
+		print ("Collison");
+	}
+
+}
