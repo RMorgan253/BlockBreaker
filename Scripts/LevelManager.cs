4cd5c63c535726429adce9f8858cc74d7fbf2cf1
Ryan Morgan <Morganglassworks@gmail.com>
2017-07-13 15:45:42 -0700

    before last lessons to finish game
    back-up of working version before upgrading components

---
+++ Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level Load Requested for: " + name);
+		Application.LoadLevel(name); 
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Player would like to quit");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	public void BrickDestroyed(){
+		if (Brick.breakableCount <= 0) {
+		LoadNextLevel();
+		}
+	}
+
+}
---
+++ Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
+
+// Below is an example of a singleton
+	void Awake () {
+		if (instance != null) {
+			Destroy (gameObject);
+			print ("Duplicate music player self-destructing!");
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
